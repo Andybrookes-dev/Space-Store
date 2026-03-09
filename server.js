82ba1115dbf98ca4717502d9660e84c149349b21
AndyMusicproducer <andybrookes78@icloud.com>
2026-03-09 12:47:45 +0000

    Add root route to serve homepage

--- server.js
+++ server.js
@@ -680,6 +680,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT ||10000;
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
